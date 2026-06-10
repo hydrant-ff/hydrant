07647d4101a8a2837c00d12a79af06c16514ab75
hydrant-ff <feuerwehrguennigfeldgetraenke@gmail.com>
2026-06-10 22:18:38 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://wnotgdoszazajchqziav.supabase.co",
+  "https://wnotgdoszazajchqziav.supabase.co/rest/v1/",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indub3RnZG9zemF6YWpjaHF6aWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMTE3NDEsImV4cCI6MjA5NjY4Nzc0MX0.mapown-zcnhsmD32_18hvWV9n0Ru8QBCoSDCdK_ZQOs"
 );
 
